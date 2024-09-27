b0b19b05cb60c5e8f20d3d0e6ef28697c19387a9
AnastasiiaPerevytska <anastasiia.zelenina.1995@gmail.com>
2024-09-27 18:14:27 +0300

    added check 4 empty field

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,9 +1,14 @@
 import s from "./SearchBar.module.css";
+import { toast } from 'react-hot-toast';
 
 export default function SearchBar({ onSubmit }) {
   const handleSubmit = (event) => {
     event.preventDefault();
     const searchQuery = event.target.query.value.trim();
+    if (!searchQuery) {
+      toast.error('The search field cannot be empty!');
+      return;
+    }
     onSubmit(searchQuery);
   };
 
